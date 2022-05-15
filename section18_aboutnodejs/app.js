96d868768714603a8f4df0853fb7ccefed5527e2
stufit <rhksdud230@dankook.ac.kr>
2022-05-15 18:18:27 +0900

    섹션18 끝

--- section18_aboutnodejs/app.js
+++ section18_aboutnodejs/app.js
@@ -1,4 +1,8 @@
+const { response } = require('express');
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 // 모든 요청에 적용되며, 자바스크립트 객체로 변환해준다.
@@ -15,9 +19,34 @@ app.get('/',(req,res)=>{
 });
 
 app.post('/store-user',(req,res)=>{
-    const userName = req.body.username
+    const userName = req.body.username;
+    const filePath = path.join(__dirname, 'data','users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    existingUsers.push(userName);
+
+    fs.writeFileSync(filePath,JSON.stringify(existingUsers));
+
     console.log(userName)
     res.redirect('/')
+    
+})
+
+app.get('/users',(req,res)=>{
+    const filePath = path.join(__dirname, 'data','users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    let responseData = '<ul>';
+
+    for (const user of existingUsers){
+        responseData += '<li>' + user + '</li>';
+    }
+    responseData +='</ul>';
+
+    res.send(responseData);
 })
 
 app.listen(3000)
