01c03353036c68082b811db303abd75be7139fab
stufit <rhksdud230@dankook.ac.kr>
2022-05-23 01:28:42 +0900

    377. 노드js에서 코드 가져오기 & 내보내기

--- section20_Router/app.js
+++ section20_Router/app.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+
 const express = require('express');
 const uuid = require('uuid');
+
+const resData = require('./util/restaurant-data');
+
 const app = express();
 
 // set 은 익스프레스 앱에 대한 특정 옵션을 설정할 수 있는 메서드이다.
@@ -14,9 +18,17 @@ app.use(express.static('public'));
 // json형태로 받아온다.
 app.use(express.urlencoded({extended:false}))
 
+
+// 전역에 미들웨어를 선언하기 때문에 꼭 4개의 파라미터를 받아야함(next())
+app.use((err,req,res,next)=>{
+    res.status(500).render('500');
+    next();
+});
+
 app.get('/',(req,res)=>{
     res.render('index');
 });
+
 
 app.get('/recommend',(req,res)=>{
     res.render('recommend');
@@ -27,11 +39,11 @@ app.post('/recommend',(req,res)=>{
     // npm i uuid 로 고유 아이디 할당.
     restaurant.id = uuid.v4();
     // util 폴더에 filepath 관련 모듈 설정함.
-    const restaurants = getStoredRestaurants();
+    const restaurants = resData.getStoredRestaurants();
 
     restaurants.push(restaurant);
 
-    storeRestaurants(restaurants);
+    resData.storeRestaurants(restaurants);
 
     
 
@@ -39,23 +51,21 @@ app.post('/recommend',(req,res)=>{
 })
 
 app.get('/restaurants',(req,res)=>{
-    const filePath = path.join(__dirname,'data','restaurants.json');
+    const storedRestaurants = resData.getStoredRestaurants();
 
-    const fileData = fs.readFileSync(filePath);
-    const storeRestaurants = JSON.parse(fileData);
+
+
     res.render('restaurants',
-    {numberOfRestaurants: storeRestaurants.length, 
-     restaurants: storeRestaurants}); // render시 ejs로 보낼 객체를 선언할 수 있다.
+    {numberOfRestaurants: storedRestaurants.length, 
+     restaurants: storedRestaurants}); // render시 ejs로 보낼 객체를 선언할 수 있다.
 });
 
 app.get('/restaurants/:id',(req,res)=>{
     const restaurantId = req.params.id
-    const filePath = path.join(__dirname,'data','restaurants.json');
+    // util에 있는 restaurant-data 참조
+    const storedRestaurants = resData.getStoredRestaurants();
 
-    const fileData = fs.readFileSync(filePath);
-    const storeRestaurants = JSON.parse(fileData);
-
-    storeRestaurants.forEach(restaurant => {
+    storedRestaurants.forEach(restaurant => {
         if (restaurant.id === restaurantId){
             // 노란색 restaurant는 foreach 의 restaurant를 뜻하며, 흰색 restaurant는 ejs로 보낼 키를 뜻함.
             return res.render('restaurant-detail',{rid : restaurantId,restaurant : restaurant});
@@ -72,17 +82,10 @@ app.get('/about',(req,res)=>{
     res.render('about')
 });
 
-
 app.use((req,res)=>{
     console.log('404에러발생')
     res.status(404).render('404');
 });
 
-
-// 전역에 미들웨어를 선언하기 때문에 꼭 4개의 파라미터를 받아야함(next())
-app.use((err,req,res,next)=>{
-    res.status(500).render('500');
-    next();
-});
 
 app.listen(3000);
--- section20_Router/util/restaurant-data.js
+++ section20_Router/util/restaurant-data.js
@@ -1,4 +1,8 @@
-const filePath = path.join(__dirname,'data','restaurants.json');
+const fs = require('fs');
+const path = require('path');
+
+// .. 을 추가하여 상위 디렉토리로 이동
+const filePath = path.join(__dirname,'..','data','restaurants.json');
 
 function getStoredRestaurants(){
 
@@ -11,3 +15,10 @@ function getStoredRestaurants(){
 function storeRestaurants(storableRestaurants){
     fs.writeFileSync(filePath,JSON.stringify(storableRestaurants));
 }
+
+// key값 : 다른 파일에서 해당 함수를 참조할 때 사용되는 변수명(내 마음대로 선언 가능)
+// value값 : 실제 사용되는 함수명
+module.exports = {
+    getStoredRestaurants : getStoredRestaurants,
+    storeRestaurants     : storeRestaurants
+};
