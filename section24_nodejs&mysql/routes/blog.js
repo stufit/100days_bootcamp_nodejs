72324dae7ac90dc86767fee88f018f4ddf556ffa
stufit <rhksdud230@dankook.ac.kr>
2022-05-28 22:38:09 +0900

    432. 단일 블로그 게시물 가져오기 & 표시

--- section24_nodejs&mysql/routes/blog.js
+++ section24_nodejs&mysql/routes/blog.js
@@ -38,5 +38,19 @@ router.post('/posts',async(req,res)=>{
     res.redirect('/posts');
 });
 
+router.get('/posts/:id', async(req,res)=>{
+    const query = `
+        SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+        inner join authors on posts.author_id = authors.id
+        WHERE posts.id = ?
+    `;
+    const [posts] = await db.query(query,[req.params.id]);
+
+    if (!posts || posts.length ===0){
+        return res.status(404).render('404');
+    }
+    res.render('post-detail',{post:posts[0]});
+});
+
 
 module.exports = router;
