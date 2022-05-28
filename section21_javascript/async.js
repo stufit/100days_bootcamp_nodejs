69fea55bd044c4a6a35b738ff4df48ba535414ef
stufit <rhksdud230@dankook.ac.kr>
2022-05-28 13:53:33 +0900

    395. 비동기(promise)

--- section21_javascript/async.js
+++ section21_javascript/async.js
@@ -1,6 +1,6 @@
-
+/*
+// [1] 비동기함수
 const fs = require('fs');
-
 function readFile(){
     let fileData;
     // 비동기에는 콜백함수가 붙기 때문에 콜백함수 안에 파라미터값(error, fileData)을 전달하여 그 안에서 돌려야함.
@@ -14,5 +14,19 @@ function readFile(){
 }
 
 readFile();
+*/
 
+// [2] 프로미스 : 일반 비동기 콜백함수 지옥을 탈출할 수 있음(구조화된 작업)
 
+const fs = require('fs/promises');
+
+function readFile(){
+    let fileData;
+    fs.readFile('./section21_javascript/data.txt')
+    .then(function(fileData){
+        console.log(fileData.toString());
+        console.log('파일 파싱 끝2');
+    })
+    console.log('hi there');
+}
+readFile();
