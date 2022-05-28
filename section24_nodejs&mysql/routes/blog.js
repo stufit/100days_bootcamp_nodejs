0e45b75aa40b8252dbc7efba630b2e2602fa94d2
stufit <rhksdud230@dankook.ac.kr>
2022-05-28 23:32:26 +0900

    433. 가져온 데이터 형식 지정 & 변환

--- section24_nodejs&mysql/routes/blog.js
+++ section24_nodejs&mysql/routes/blog.js
@@ -49,7 +49,18 @@ router.get('/posts/:id', async(req,res)=>{
     if (!posts || posts.length ===0){
         return res.status(404).render('404');
     }
-    res.render('post-detail',{post:posts[0]});
+     const postData = {
+        ...posts[0],
+        date: posts[0].date.toISOString(),
+        humanReadableDate : posts[0].date.toLocaleDateString('en-US',{
+            weekday: 'long',
+            year : 'numeric',
+            month : 'long',
+            day : 'numeric'
+        }),
+     };
+
+    res.render('post-detail',{post:postData});
 });
 
 
