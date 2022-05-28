ccdebffdc4c795142dedc0b648787a4ca8c94847
stufit <rhksdud230@dankook.ac.kr>
2022-05-28 15:39:59 +0900

    397. 비동기 async & await

--- section21_javascript/async.js
+++ section21_javascript/async.js
@@ -16,6 +16,8 @@ function readFile(){
 readFile();
 */
 
+
+/*
 // [2] 프로미스 : 일반 비동기 콜백함수 지옥을 탈출할 수 있음(구조화된 작업)
 
 const fs = require('fs/promises');
@@ -27,6 +29,28 @@ function readFile(){
         console.log(fileData.toString());
         console.log('파일 파싱 끝2');
     })
+    // promise 에서는 try-catch는 사용할 수 없다. promise는 then에서 항상 성공만 가져가기 때문에 catch를 붙여서 실패시 에러확인
+    .catch(function(error){ 
+        console.log(error);
+    });
     console.log('hi there');
+}
+readFile();
+*/
+
+// [2] async & await : promise와 동일하지만 then을 사용하지 않고, try-catch 구문 사용 가능하다.
+
+const fs = require('fs/promises');
+
+async function readFile(){
+    let fileData;
+    try{
+        fileData = await fs.readFile('./section21_javascript/data.txt')
+    } catch(err){
+        console.log(err)
+    }
+        console.log(fileData.toString());
+        console.log('파일 파싱 끝2');
+        console.log('hi there');
 }
 readFile();
