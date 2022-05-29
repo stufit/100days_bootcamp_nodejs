32070b4927d317b663436724724d76821f65581b
stufit <rhksdud230@dankook.ac.kr>
2022-05-30 01:10:57 +0900

    457. 문서 가져오기(/posts) & 표시

--- section26_nodejs_mongodb/routes/blog.js
+++ section26_nodejs_mongodb/routes/blog.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const mongodb = require('mongodb');
+// mongodb의 id를 사용하기 위해서는 해당 변수 선언
+const ObjectId = mongodb.ObjectId;
 const db = require('../data/database');
 
 const router = express.Router();
@@ -8,8 +10,15 @@ router.get('/', function(req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get('/posts', async(req, res)=> {
+  //const posts = await db.getDb().collection('posts').find({},{title:1, summary:1,'author.name':1}).toArray();
+  // 위의 구문은 mongo shell에서 작성할 때 사용되므로 nodejs에서는 아래와 같이 작업한다.(위의 것도 작동되기는함.)
+  // nodejs에서는 find({}).project({}) 로 사용됨.
+  const posts = await db.getDb().collection('posts')
+  .find({})
+  .project({title:1, summary:1,'author.name':1}).toArray();
+  console.log(posts)
+  res.render('posts-list',{posts:posts});
 });
 
 router.get('/new-post', async function(req, res) {
@@ -17,5 +26,30 @@ router.get('/new-post', async function(req, res) {
   //console.log(authors)
   res.render('create-post',{authors:authors});
 });
+
+router.post('/posts', async (req,res)=>{
+  // trim()을 붙이지 않으면 다음과 같은 에러가 발생함.
+  // BSONTypeError: Argument passed in must be a string of 12 bytes or a string of 24 hex characters or an integer
+  const authorId = new ObjectId(req.body.author.trim());
+  const author = await db.getDb().collection('authors').findOne({_id: authorId});
+
+  const newPost = {
+    title : req.body.title,
+    summary : req.body.summary,
+    body : req.body.content,
+    date : new Date(),
+    author : {
+      id : authorId,
+      name : author.name,
+      email : author.email
+
+    }
+  };
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+  res.redirect('/posts');
+});
+
 
 module.exports = router;
