629f5e255a498df4f29342a6373b2b0e52854c84
stufit <rhksdud230@dankook.ac.kr>
2022-06-03 00:40:17 +0900

    461. 게시물 업데이트

--- section26_nodejs_mongodb/routes/blog.js
+++ section26_nodejs_mongodb/routes/blog.js
@@ -54,12 +54,44 @@ router.post('/posts', async (req,res)=>{
 router.get('/posts/:id', async(req,res)=>{
   const postId = req.params.id;
   const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)},{summary:0});
-
+  
   if(!post){
     return res.status(404).render('404');
   }
+
+  post.humanReadableDate = post.date.toLocaleDateString('en-US',{
+    weekday: 'long',
+    year : 'numeric',
+    month : 'long',
+    day : 'numeric',
+  });
+  post.date = post.date.toISOString();
+
   res.render('post-detail',{post:post})
 
 });
+
+router.get('/posts/:id/edit', async(req,res)=>{
+  const postId = req.params.id;
+  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)},{title:1,summary:1,body:1});
+  if(!post){
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post',{post:post});
+
+});
+
+router.post('/posts/:id/edit', async(req,res)=>{
+  const postId = new ObjectId(req.params.id);
+  const result = await db.getDb().collection('posts').updateOne({_id : postId},{$set :{
+    title : req.body.title,
+    summary : req.body.summary,
+    body : req.body.content
+  }});
+
+  res.redirect('/posts');
+});
+
 
 module.exports = router;
